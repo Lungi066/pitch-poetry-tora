57f5bb080fd10407c7a3adb114772423e1111c65
lungisani ngcobo <mapholoba066@gmail.com>
2024-03-28 22:57:28 +0200

    add assets

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,6 +1,11 @@
+import { z } from "zod"
+
 import { Button } from "@/components/ui/button"
 
 const SignupForm = () => {
+  const formSchema = z.object({
+    username: z.string().min(2).max(50),
+  })
   return (
     <div>
       <Button>Click me</Button>
